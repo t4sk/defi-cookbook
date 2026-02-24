7850faaddc5cea84ce336a295b7daf725cd4e7a6
t4sk <tsk.nakamura@gmail.com>
2026-02-24 07:57:37 +0100

    tmp

--- src/insurance/Stake.sol
+++ src/insurance/Stake.sol
@@ -9,20 +9,30 @@ contract Stake {
     using SafeTransfer for IERC20;
 
     // TODO: events
+    // TODO: gas golf
 
+    // Rate scale
+    uint256 private constant R = 1e18;
     IERC20 public immutable token;
 
     bool public stopped;
     mapping(address => bool) public auths;
 
-    // Last updated timestamp
-    uint256 public up;
-    // Expiration
+    // Total staked
+    uint256 public total;
+    // user => staked amount
+    mapping(address usr => uint256 amt) public shares;
+
+    // Last updated time
+    uint256 public last;
+    // Expiration time
     uint256 public exp;
-    // Rate per second
+    // Rate of token emission per second
     uint256 public rate;
     // Rate accumulator
     uint256 public acc;
+    mapping(address usr => uint256 acc) public accs;
+    mapping(address usr => uint256 amt) public rewards;
 
     modifier auth() {
         require(auths[msg.sender], "not auth");
@@ -34,17 +44,12 @@ contract Stake {
         _;
     }
 
-    modifier paused() {
-        require(stopped, "not stopped");
-        _;
-    }
-
     constructor(address _token) {
         token = IERC20(_token);
         // TODO:emit
         auths[msg.sender] = true;
-        updatedAt = block.timestamp;
-        expAt = block.timestamp;
+        last = block.timestamp;
+        exp = block.timestamp;
     }
 
     function allow(address usr) external auth {
@@ -55,27 +60,66 @@ contract Stake {
         auths[usr] = false;
     }
 
-    function deposit() external auth live {
-        // queue -> delay -> deposit
-        // not stopped
+    function deposit(address usr, uint256 amt) external auth live {
+        // TODO: delay -> deposit
+        require(block.timestamp < exp, "expired");
+        // TODO: handle rebase token (inf deposit + checking bal diff)?
+        token.safeTransferFrom(msg.sender, address(this), amt);
+
+        // TODO: restake
+        updateRewards(usr);
+        total += amt;
+        shares[usr] += amt;
     }
 
-    function withdraw() external auth live {
-        // queue -> delay -> withdraw
-        // not stopped
+    function withdraw(address usr, uint256 amt) external auth live {
+        // TODO: delay -> withdraw
+        updateRewards(usr);
+        total -= amt;
+        shares[usr] -= amt;
+
+        // TODO: handle rebase token (inf deposit + checking bal diff)?
+        token.safeTransfer(usr, amt);
     }
 
-    function drip() public {
-        uint256 dt = block.timestamp - up;
+    function calcRewards(address usr) public view returns (uint256) {
+        uint256 t = Math.min(block.timestamp, exp);
+        uint256 a = acc;
+        if (total > 0) {
+            a += rate * (t - last) * R / total;
+        }
+        return rewards[usr] + shares[usr] * (a - accs[usr]) / R;
     }
 
-    // TODO: auth?
+    function updateRewards(address usr) public returns (uint256 amt) {
+        uint256 t = Math.min(block.timestamp, exp);
+        // TODO: if total = 0?
+        if (total > 0) {
+            acc += rate * (t - last) * R / total;
+        }
+        last = t;
+
+        amt = shares[usr] * (acc - accs[usr]) / R;
+        accs[usr] = acc;
+        rewards[usr] += amt;
+    }
+
+    function getRewards(address usr) public returns (uint256 amt) {
+        // TODO: updateRewards(usr);?
+        amt = rewards[usr];
+        if (amt > 0) {
+            rewards[usr] = 0;
+            token.safeTransfer(usr, amt);
+        }
+    }
+
     function pay() external {}
 
     // TODO
     // function extend() external {}
 
-    function cut() external auth paused {
+    function cut() external auth {
+        require(stopped, "not stopped");
         // TODO: stopped?
         // delete withdrawal queue + pull funds from withdrawal queue
     }
@@ -86,11 +130,6 @@ contract Stake {
     // restake (get rewards + deposit (skip withdrawal queue))
 
     // TODO: emergency clean up
-
-    function go() external auth paused {
-        stopped = false;
-    }
-
     function stop() external auth live {
         stopped = true;
     }
